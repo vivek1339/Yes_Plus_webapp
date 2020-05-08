edeaa7c716ed1442b1ccdf7a3fbdbfcdea54bfb7
Poojitha-maroor <59948147+Poojitha-maroor@users.noreply.github.com>
2020-05-08 18:16:47 +0530

    Added css file

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ app.use(morgan('dev'));
 //app.use('/',express.static(__dirname)+'/public');
 
 app.use(express.static(__dirname+'src/html'));
+app.use('/css',express.static(__dirname +'src/html/css'));
 
 app.use((req,res,next)=>{
     res.statusCode=200;
     res.setHeader('Content-Type','text/html');
-    res.end('<html><body><h1>Express server</h1></body</html');
+    res.end('<html><body><h1>Express server</h1></body</html>');
 });
 
 const server=http.createServer(app);

b48beceb6869661220f3966804ce51fc868ec636
amrutha252000 <53553240+amrutha252000@users.noreply.github.com>
2020-06-20 13:34:05 +0530

    updated home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Welcome from './home/welcome';
 import Explain from './home/explain';
 import Benefit from './home/benefit';
+import Images from './home/images';
+import Activity from './home/activity';
 
 const Home = () =>(
     <div>
         <Welcome />
         <Explain />
         <Benefit />
+        <Images />
+        <Activity />
     </div>
 )
 

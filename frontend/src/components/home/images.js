bcbf29ade20e8a561db993b9de0159b5973799c2
nehagupta4424 <64189132+nehagupta4424@users.noreply.github.com>
2020-06-21 10:11:04 +0530

    added images component

--- frontend/src/components/home/images.js
+++ frontend/src/components/home/images.js
@@ -1,1 +1,31 @@
 
+import React from 'react';
+
+const images = () =>{
+    return(
+
+<div classname="size">
+        <div className="carousel">
+            <div className="carousel-item">
+                <img src="./images/img7.jpg"  alt=""/> 
+            </div>
+            <div className="carousel-item">
+                <img src="./images/img8.jpg" alt=""/> 
+            </div>
+            <div className="carousel-item">
+                <img src="./images/img9.jpg" alt=""/> 
+            </div>
+            <div className="carousel-item">
+                <img src="./images/img10.jpg" alt=""/> 
+            </div>
+            <div className="carousel-item">
+                <img src="./images/img11.jpg" alt=""/> 
+            </div>
+            <div className="carousel-item">
+                <img src="./images/img12.jpg" alt=""/> 
+            </div>
+        </div>
+        </div>
+        );
+        };
+        export default images;

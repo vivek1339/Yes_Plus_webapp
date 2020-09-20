43d209db1b9e65642e5879a17732d4f929f76992
nehagupta4424 <64189132+nehagupta4424@users.noreply.github.com>
2020-09-20 17:21:21 +0530

    Update popup.js

--- frontend/src/components/home/popup.js
+++ frontend/src/components/home/popup.js
@@ -1,15 +1,15 @@
 import React from 'react';
 const Popup = () =>{
         return (
-        <div class="yp_popup">
-	        <div class="modal fade" id="pop-up" tabindex="-1" role="dialog">
-  	            <div class="modal-dialog" role="document">
-    	            <div class="modal-content">
-    	                <div class="modal-body">
+        <div className="yp_popup">
+	        <div className="modal fade" id="pop-up" tabindex="-1" role="dialog">
+  	            <div className="modal-dialog" role="document">
+    	            <div className="modal-content">
+    	                <div className="modal-body">
     		              <button type="button" className="close" data-dismiss="modal" aria-label="Close"> 
       		                    <span aria-hidden="true" >&times;</span>
         		            </button>
-						<div class="popup-img"><img src="/images/certificate.jpg" alt="" width="100" height="100"/></div>
+						<div className="popup-img"><img src="/images/certificate.jpg" alt="" width="100" height="100"/></div>
         		          <p>You Will Earn a Certificate which is used to your Resume. <br></br>
 							  Resume is a place where you can show your extra curricular activities 
 							  and co-curricular activities So, dont miss it!!
